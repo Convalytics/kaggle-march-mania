37d9b874ecf2375452ea2fcbd2119bbd043f48e3
Jason Green <jason@convalytics.com>
2014-02-12 23:43:47 -0500

    Summarizing Data
    Starting to summarize data.

--- Kaggle_March_Mania.r
+++ Kaggle_March_Mania.r
@@ -5,9 +5,12 @@
 #  https://github.com/Convalytics/kaggle-march-mania
 ######################################################
 
+#install.packages('plyr')
+library(plyr)
 
 # Set Working Directory
-setwd("C:\\Users\\jgreen\\Documents\\GitHub\\kaggle-march-mania")
+#setwd("C:\\Users\\jgreen\\Documents\\GitHub\\kaggle-march-mania")
+setwd("~/GitHub/kaggle-march-mania")
 
 # Import Data
 regular_season_results <- read.csv("~/GitHub/kaggle-march-mania/regular_season_results.csv")
@@ -18,7 +21,17 @@ submission <- sample_submission
 submission$lowTeam <- substr(submission$id,3,5)
 submission$highTeam <- substr(submission$id,7,9)
 
+
+# Summaries
+summary(regular_season_results)
+head(regular_season_results, n=5)
+
+
+table(regular_season_results$wteam, regular_season_results$season)
+count(regular_season_results$wteam,regular_season_results$season)
+
 # Get total win % for each team
+
 
 # Get tourney win count for each team
 
@@ -27,6 +40,8 @@ submission$highTeam <- substr(submission$id,7,9)
 # Does high or low team have better tourney performance?
 
 # Who performs better when away?
+
+# Account for likelihood of an upset.
 
 
 
